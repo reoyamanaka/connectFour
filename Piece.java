9707a1e9aa7e176cc87b735b84bea83abbdcfb0c
Reo Yamanaka <ryamanaka807@gmail.com>
2021-08-14 18:47:01 -0400

    created Piece class

---
+++ Piece.java
@@ -0,0 +1,14 @@
+public class Piece {
+
+    // instance variables
+    private int x;
+    private int y;
+    private char type;
+
+    // constructor
+    public Piece (int x, int y, char type) {
+        x = x;
+        y = y;
+        type = type;
+    }
+}
