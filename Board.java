140a15632b891d476ecd64a6437af92bdbdffe33
Reo Yamanaka <ryamanaka807@gmail.com>
2021-08-14 19:47:39 -0400

    modified board to be a 2D array

--- Board.java
+++ Board.java
@@ -3,25 +3,31 @@ public class Board {
     // instance variables
     private int width;
     private int height;
-    private String field;
+    private String[][] field;
 
     // constructor
-    public Board(int initWidth, int initHeight) {
-        width = initWidth;
-        height = initHeight;
-        String gameBoard = "";
+    public Board(int width, int height) {
+        String[] row = new String[width];
+        String[][] gameBoard = new String[height][width];
+        for (int i = 0; i < width; i++) {
+            row[i] = "-";
+        }
         for (int i = 0; i < height; i++) {
-            String row = "";
-            for (int j = 0; j < width; j++) {
-                row += "-";
-            }
-            row += "\n";
-            gameBoard += row;
+            gameBoard[i] = row;
         }
         field = gameBoard;
     }
     
     public String getField() {
-        return this.field;
+        String board = "";
+        for (int i = 0; i < field.length; i++) {
+            String boardRow = "";
+            for (int j = 0; j < field[i].length; j++) {
+                boardRow += field[i][j];
+            }
+            boardRow += "\n";
+            board += boardRow;
+        }
+        return board;
     }
 }
